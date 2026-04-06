f7af643c70445d3cc5e5bd8889b2b591caef1ef3
dasttyasu <hpfe220@outlook.com>
2026-04-07 02:26:03 +0800

    Add amlogic and PassWall2 feed sources
    Added additional feed sources for amlogic and PassWall2.

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -9,6 +9,16 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+# Add amlogic feed source
+echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic' >> feeds.conf.default
+
+# PassWall2 frontend feed (official frontend project)
+echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2' >> feeds.conf.default
+
+# PassWall2 runtime dependencies feed (separated from frontend)
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >> feeds.conf.default
+
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
+
 
